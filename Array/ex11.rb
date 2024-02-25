e66b66ef7df71e2eb54bc8c93f874fab5cafe2e6
lamvd <vuduclam2412@gmail.com>
2024-02-25 18:10:12 +0700

    Last study

--- Array/ex11.rb
+++ Array/ex11.rb
@@ -1,16 +1,16 @@
 # 11. Khởi tạo một mảng gồm n (n≤ 99) số tự nhiên sau đó đảo ngược các phần tử trong mảng.
 
 begin
-    puts 'Nhap so phan tu mang (so nguyen <=99) n = '
-    while arraySize = Integer(gets.chomp)
-      if (arraySize < 100 && arraySize > 0)
-        break
-      end
-      puts 'Vui long nhap so nguyen <=99'
+  puts 'Nhap so phan tu mang (so nguyen <=99) n = '
+  while arraySize = Integer(gets.chomp)
+    if (arraySize < 100 && arraySize > 0)
+      break
     end
-rescue
     puts 'Vui long nhap so nguyen <=99'
-    retry
+  end
+rescue
+  puts 'Vui long nhap so nguyen <=99'
+  retry
 end
 myArray = Array.new(arraySize) { rand(1...9) }
 
